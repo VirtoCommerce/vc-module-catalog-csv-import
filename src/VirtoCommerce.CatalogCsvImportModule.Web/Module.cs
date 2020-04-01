9fe618151df2110c8dd9652ead966bce639057e0
Andrey Kozlov <yecli.github@gmail.com>
2020-04-01 16:33:30 +0500

    VP-1583: Remove  redundant module interfaces

--- src/VirtoCommerce.CatalogCsvImportModule.Web/Module.cs
+++ src/VirtoCommerce.CatalogCsvImportModule.Web/Module.cs
@@ -1,19 +1,14 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.CatalogCsvImportModule.Core;
 using VirtoCommerce.CatalogCsvImportModule.Core.Services;
 using VirtoCommerce.CatalogCsvImportModule.Data.Services;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.Platform.Core.ExportImport;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.CatalogCsvImportModule.Web
 {
-    public class Module : IModule, IExportSupport, IImportSupport
+    public class Module : IModule
     {
         public ManifestModuleInfo ModuleInfo { get; set; }
         public void Initialize(IServiceCollection serviceCollection)
@@ -30,17 +25,7 @@ namespace VirtoCommerce.CatalogCsvImportModule.Web
 
         public void Uninstall()
         {
-            throw new NotImplementedException();
-        }
-
-        public Task ExportAsync(Stream outStream, ExportImportOptions options, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ImportAsync(Stream inputStream, ExportImportOptions options, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
+            // No need in actions
         }
     }
 }
