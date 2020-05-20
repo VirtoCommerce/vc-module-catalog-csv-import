203d09bd947ba15a017307f5d078710f59125cde
alexandr andreev <alivemenone@gmail.com>
2020-05-20 16:38:58 +0300

    VP-2309: Make ErrorCount computable (#43)

--- src/VirtoCommerce.CatalogCsvImportModule.Web/Model/PushNotifications/JobNotificationBase.cs
+++ src/VirtoCommerce.CatalogCsvImportModule.Web/Model/PushNotifications/JobNotificationBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using VirtoCommerce.Platform.Core.PushNotifications;
@@ -8,52 +8,53 @@ namespace VirtoCommerce.CatalogCsvImportModule.Web.Model.PushNotifications
     /// <summary>
     /// Base class for sending jobs status notifications.
     /// </summary>
-	public class JobNotificationBase : PushNotification
-	{
-		public JobNotificationBase(string creator)
-			: base(creator)
-		{
-			Errors = new List<string>();
-		}
+    public class JobNotificationBase : PushNotification
+    {
+        public JobNotificationBase(string creator)
+            : base(creator)
+        {
+            Errors = new List<string>();
+        }
         /// <summary>
         /// Gets or sets the job finish date and time.
         /// </summary>
         /// <value>
         /// The finished.
         /// </value>
-		[JsonProperty("finished")]
-		public DateTime? Finished { get; set; }
+        [JsonProperty("finished")]
+        public DateTime? Finished { get; set; }
         /// <summary>
         /// Gets or sets the total count of objects to process.
         /// </summary>
         /// <value>
         /// The total count.
         /// </value>
-		[JsonProperty("totalCount")]
-		public long TotalCount { get; set; }
+        [JsonProperty("totalCount")]
+        public long TotalCount { get; set; }
         /// <summary>
         /// Gets or sets the count of processed objects.
         /// </summary>
         /// <value>
         /// The processed count.
         /// </value>
-		[JsonProperty("processedCount")]
-		public long ProcessedCount { get; set; }
+        [JsonProperty("processedCount")]
+        public long ProcessedCount { get; set; }
+
         /// <summary>
-        /// Gets or sets the count of errors during processing.
+        /// Gets the count of errors during processing.
         /// </summary>
         /// <value>
         /// The error count.
         /// </value>
-		[JsonProperty("errorCount")]
-		public long ErrorCount { get; set; }
+        [JsonProperty("errorCount")]
+        public long ErrorCount => Errors?.Count ?? 0;
         /// <summary>
         /// Gets or sets the errors that has occurred during processing.
         /// </summary>
         /// <value>
         /// The errors.
         /// </value>
-		[JsonProperty("errors")]
-		public ICollection<string> Errors { get; set; }
-	}
+        [JsonProperty("errors")]
+        public ICollection<string> Errors { get; set; }
+    }
 }
