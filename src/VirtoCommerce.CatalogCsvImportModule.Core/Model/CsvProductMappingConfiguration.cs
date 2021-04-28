66332211bfbd6f3f73fefde2d4de9fc53669aa32
Oleg Zhuk <zhukoo@gmail.com>
2021-04-28 15:15:46 +0200

    Add StartDate, EndDate columns to Column Mapping (#74)

--- src/VirtoCommerce.CatalogCsvImportModule.Core/Model/CsvProductMappingConfiguration.cs
+++ src/VirtoCommerce.CatalogCsvImportModule.Core/Model/CsvProductMappingConfiguration.cs
@@ -24,11 +24,11 @@ namespace VirtoCommerce.CatalogCsvImportModule.Core.Model
             var requiredFields = new List<string>();
             var optionalFields = ReflectionUtility.GetPropertyNames<CsvProduct>(x => x.Name, x => x.Id, x => x.Sku, x => x.CategoryPath, x => x.CategoryId, x => x.MainProductId, x => x.PrimaryImage, x => x.AltImage, x => x.SeoUrl, x => x.SeoTitle,
                                                                                 x => x.SeoDescription, x => x.SeoLanguage, x => x.SeoStore, x => x.Review, x => x.ReviewType, x => x.IsActive, x => x.IsBuyable, x => x.TrackInventory,
-                                                                                x => x.PriceId, x => x.SalePrice, x => x.ListPrice, x=>x.PriceMinQuantity, x => x.Currency, x => x.PriceListId, x => x.Quantity,
-                                                                                x => x.FulfillmentCenterId, x => x.PackageType, x=> x.OuterId, x=>x.Priority, x=>x.MaxQuantity, x=>x.MinQuantity,
+                                                                                x => x.PriceId, x => x.SalePrice, x => x.ListPrice, x => x.PriceMinQuantity, x => x.Currency, x => x.PriceListId, x => x.Quantity,
+                                                                                x => x.FulfillmentCenterId, x => x.PackageType, x => x.OuterId, x => x.Priority, x => x.MaxQuantity, x => x.MinQuantity,
                                                                                 x => x.ManufacturerPartNumber, x => x.Gtin, x => x.MeasureUnit, x => x.WeightUnit, x => x.Weight,
                                                                                 x => x.Height, x => x.Length, x => x.Width, x => x.TaxType, x => x.ProductType, x => x.ShippingType,
-                                                                                x => x.Vendor, x => x.DownloadType, x => x.DownloadExpiration, x => x.HasUserAgreement , x=>x.MaxNumberOfDownload);
+                                                                                x => x.Vendor, x => x.DownloadType, x => x.DownloadExpiration, x => x.HasUserAgreement, x => x.MaxNumberOfDownload, x => x.StartDate, x => x.EndDate);
 
             retVal.PropertyMaps = requiredFields.Select(x => new CsvProductPropertyMap { EntityColumnName = x, CsvColumnName = x, IsRequired = true }).ToList();
             retVal.PropertyMaps.AddRange(optionalFields.Select(x => new CsvProductPropertyMap { EntityColumnName = x, CsvColumnName = x, IsRequired = false }));
